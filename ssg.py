309794d50049a3381be73adc1407ce9897d92b9c
Yan Xu <yan.xu@sas.com>
2023-04-11 19:13:21 +0000

    Completed module

--- ssg.py
+++ ssg.py
@@ -4,7 +4,9 @@ import ssg.parsers
 
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser(),]}
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser(), 
+                                                          ssg.parsers.MarkdownParser(), 
+                                                          ssg.parsers.ReStructuredTextParser()]}
     Site(**config).build()
 
 
