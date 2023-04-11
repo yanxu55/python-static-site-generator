c88300cb40e325b597efcaebbff1b0d2c85a7e64
Yan Xu <yan.xu@sas.com>
2023-04-11 14:51:39 +0000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Parser:
     extensions: List[str] = []
     
-    def valid_extensions(self, extension): 
+    def valid_extension(self, extension): 
         return extension in self.extensions
     
     def parse(self, path: Path, source: Path, dest: Path):
