0b88fca0ff01dd94d13df14321b8902b783754d6
Yan Xu <yan.xu@sas.com>
2023-04-11 19:06:48 +0000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -47,6 +47,16 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
-        
-       
+
+
+def ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+
 
