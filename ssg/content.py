f542480b8466f185b6b0bda50f21faae5508bd98
Yan Xu <yan.xu@sas.com>
2023-04-11 17:14:17 +0000

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -33,6 +33,19 @@ class Content(Mapping):
     @type.setter
     def type(self, type):
         self.data['type'] = type
-        
 
+
+    def __getitem__(self, key):
+        return self.data[key]
+    
+    def __iter__(self):
+        return iter(self.data)
+    
+    def __len__(self):
+        return len(self.data)
+    
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.data!r})"
+    
     
