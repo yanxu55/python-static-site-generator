84721e016c2a163832242aaa3597a7e282e51019
Yan Xu <yan.xu@sas.com>
2023-04-11 17:07:05 +0000

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -25,10 +25,8 @@ class Content(Mapping):
     
     @property
     def type(self):
-        if('type' in self.data):
-            return self.data['type']
-        else:
-            return None
+        return self.data['type'] if "type" in self.data else None
+    
     
     
     
