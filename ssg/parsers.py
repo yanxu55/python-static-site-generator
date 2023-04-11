2c1c268a15bfd10d1795e2d8ea615483b76526f3
Yan Xu <yan.xu@sas.com>
2023-04-11 14:50:57 +0000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,3 +11,7 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+        
