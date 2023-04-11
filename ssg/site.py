dcd30445c0fb1b12a5269a026d08924bed98c0f7
Yan Xu <yan.xu@sas.com>
2023-04-11 15:04:58 +0000

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -4,7 +4,7 @@ class Site:
     def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
-        self.parsers = None
+        self.parsers = parsers or []
     
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
