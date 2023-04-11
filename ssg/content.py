5fb976ba2c692b1a8e14edb57b108f2ca6924d63
Yan Xu <yan.xu@sas.com>
2023-04-11 17:02:41 +0000

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -15,6 +15,10 @@ class Content(Mapping):
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
     
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data['content'] = content
+        
     
     
     
