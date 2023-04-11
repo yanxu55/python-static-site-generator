685114dd26e09675607c0d6e7be43c32ef5e475c
Yan Xu <yan.xu@sas.com>
2023-04-11 15:42:12 +0000

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -5,21 +5,27 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []
+
     
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
-    
+
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
 
     def load_parser(self, extension):
         for parser in self.parsers:
             if parser.valid_extension(extension):
                 return parser
+
 
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
@@ -28,5 +34,5 @@ class Site:
             parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
-            
+
 
